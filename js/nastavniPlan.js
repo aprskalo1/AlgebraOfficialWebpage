cf08a8fd8aeab0aae50357a598ec154b1964b318
unknown <ante.prskalo4@gmail.com>
2023-01-26 16:26:17 +0100

    Nastavni plan new version

--- js/nastavniPlan.js
+++ js/nastavniPlan.js
@@ -1,65 +1,97 @@
 $(() => {
-    const autoCompleteArr = [];
-    const dictKolegij = {};
+    let autoCompPolje = [];
+    let kolegijPolje = {};
 
-    const response = $.ajax({
-        url: 'https://www.fulek.com/data/api/supit/curriculum-list/hr',
-        type: 'GET',
-        dataType: 'json',
-        success: function (podaci) {
-            console.log(podaci);
-            for (let i = 0; i < podaci.data.length; i++) {
-                autoCompleteArr.push(podaci.data[i]['kolegij']);
-                dictKolegij[podaci.data[i]['kolegij']] = i + 1;
-            }
-        },
+    let Kolegij = {
+        nazivKolegija: "",
+        brojEcts: "",
+        kolicinaSati: "",
+        kolicinaPredavanja: "",
+        kolicinaVjezbi: "",
+        tipKolegija: ""
+    };
+
+    function PopuniTablicu(arg) {
+        const tbody = document.querySelector("table tbody");
+        const lastRow = tbody.querySelector("tr:last-child");
+        const newRow = document.createElement("tr");
+        newRow.innerHTML = `
+          <td>${arg.nazivKolegija}</td>
+          <td>${arg.brojEcts}</td>
+          <td>${arg.kolicinaSati}</td>
+          <td>${arg.kolicinaPredavanja}</td>
+          <td>${arg.kolicinaVjezbi}</td>
+          <td>${arg.tipKolegija}</td>
+          <td><button id="deleteRow" class="btn btn-danger">Obriši</button></td>
+        `;
+        tbody.insertBefore(newRow, lastRow);
+
+        document.querySelector("#sumECTS").textContent = Number(document.querySelector("#sumECTS").textContent) + arg.brojEcts;
+        document.querySelector("#sumHours").textContent = Number(document.querySelector("#sumHours").textContent) + arg.kolicinaSati;
+        document.querySelector("#sumClass").textContent = Number(document.querySelector("#sumClass").textContent) + arg.kolicinaPredavanja;
+        document.querySelector("#sumPrac").textContent = Number(document.querySelector("#sumPrac").textContent) + arg.kolicinaVjezbi;
+    }
+
+    fetch('https://www.fulek.com/data/api/supit/curriculum-list/hr', {
+        method: 'GET',
         headers: {
-            Authorization: 'Bearer ' + localStorage.getItem('token')
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
         }
-    });
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            for (let i = 0; i < data.data.length; i++) {
+                autoCompPolje.push(data.data[i]['kolegij']);
+                kolegijPolje[data.data[i]['kolegij']] = i + 1;
+            }
+        })
+        .catch(error => console.error('Error:', error));
 
     $('#kolegij_naziv').autocomplete({
         maxShowItems: 5,
-        source: autoCompleteArr,
+        source: autoCompPolje,
         select: function (e, ui) {
-            details = $.ajax({
-                url: 'https://www.fulek.com/data/api/supit/get-curriculum/' + dictKolegij[ui.item.label],
-                type: 'GET',
-                dataType: 'json',
-                success: function (podaci) {
-                    $('table tbody tr:last-child').before(`
-                    <tr>
-                    <td>${podaci.data.kolegij}</td>
-                    <td>${podaci.data.ects}</td>
-                    <td>${podaci.data.sati}</td>
-                    <td>${podaci.data.predavanja}</td>
-                    <td>${podaci.data.vjezbe}</td>
-                    <td>${podaci.data.tip}</td>
-                    <td><button id="deleteRow" class='btn btn-danger'>Obriši</button></td>
-                    </tr>
-                    `);
-                    $('#sumECTS').text(Number($('#sumECTS').text()) + podaci.data.ects);
-                    $('#sumHours').text(Number($('#sumHours').text()) + podaci.data.sati);
-                    $('#sumClass').text(Number($('#sumClass').text()) + podaci.data.predavanja);
-                    $('#sumPrac').text(Number($('#sumPrac').text()) + podaci.data.vjezbe);
-                },
+            fetch(`https://www.fulek.com/data/api/supit/get-curriculum/${kolegijPolje[ui.item.label]}`, {
+                method: 'GET',
                 headers: {
-                    Authorization: 'Bearer ' + localStorage.getItem('token')
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
                 }
-            });
+            })
+                .then(response => response.json())
+                .then(data => {
+                    Kolegij.nazivKolegija = data.data.kolegij;
+                    Kolegij.brojEcts = data.data.ects;
+                    Kolegij.kolicinaSati = data.data.sati;
+                    Kolegij.kolicinaPredavanja = data.data.predavanja;
+                    Kolegij.kolicinaVjezbi = data.data.vjezbe;
+                    Kolegij.tipKolegija = data.data.tip;
+                    PopuniTablicu(Kolegij);
+                })
+                .catch(error => console.error('Error:', error));
         }
     });
 
-    $("#kolegij_naziv").click(function () {
-        $(this).val("");
+    var kolegijNaziv = document.getElementById('kolegij_naziv');
+    kolegijNaziv.addEventListener('click', function () {
+        this.value = "";
     });
 
-    $('table').on('click', '#deleteRow', function () {
-        var currentRow = $(this).closest("tr");
-        $('#sumECTS').text(Number($('#sumECTS').text()) - Number(currentRow.find("td:eq(1)").text()));
-        $('#sumHours').text(Number($('#sumHours').text()) - Number(currentRow.find("td:eq(2)").text()));
-        $('#sumClass').text(Number($('#sumClass').text()) - Number(currentRow.find("td:eq(3)").text()));
-        $('#sumPrac').text(Number($('#sumPrac').text()) - Number(currentRow.find("td:eq(4)").text()));
-        $(this).closest('tr').remove();
+    var table = document.querySelector('table');
+    table.addEventListener('click', function (event) {
+        if (event.target.id === 'deleteRow') {
+            var currentRow = event.target.closest("tr");
+            var sumECTS = document.getElementById('sumECTS');
+            sumECTS.textContent = Number(sumECTS.textContent) - Number(currentRow.querySelectorAll("td")[1].textContent);
+            var sumHours = document.getElementById('sumHours');
+            sumHours.textContent = Number(sumHours.textContent) - Number(currentRow.querySelectorAll("td")[2].textContent);
+            var sumClass = document.getElementById('sumClass');
+            sumClass.textContent = Number(sumClass.textContent) - Number(currentRow.querySelectorAll("td")[3].textContent);
+            var sumPrac = document.getElementById('sumPrac');
+            sumPrac.textContent = Number(sumPrac.textContent) - Number(currentRow.querySelectorAll("td")[4].textContent);
+            currentRow.remove();
+        }
     });
 });
